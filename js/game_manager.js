d8016edf84b062e453dad3a85b414fdb6a7fa3c8
asalmon <allisonmariesalmon@gmail.com>
2014-03-20 21:32:11 -0500

    messing around with making a binary clone

--- js/game_manager.js
+++ js/game_manager.js
@@ -59,7 +59,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.9 ? 2 : 4;
+    var value = Math.random() < 0.3 ? 0 : 1;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
@@ -126,8 +126,8 @@ GameManager.prototype.move = function (direction) {
         var next      = self.grid.cellContent(positions.next);
 
         // Only one merger per row traversal?
-        if (next && next.value === tile.value && !next.mergedFrom) {
-          var merged = new Tile(positions.next, tile.value * 2);
+        if (next && (next.value & tile.value === 1) && !next.mergedFrom) {
+          var merged = new Tile(positions.next, 1);
           merged.mergedFrom = [tile, next];
 
           self.grid.insertTile(merged);
@@ -229,7 +229,7 @@ GameManager.prototype.tileMatchesAvailable = function () {
 
           var other  = self.grid.cellContent(cell);
 
-          if (other && other.value === tile.value) {
+          if (other && (other.value & tile.value === 1)) {
             return true; // These two tiles can be merged
           }
         }
